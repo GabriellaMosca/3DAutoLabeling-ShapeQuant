a13df056d59b862cb5f7788e11570bab655d4e5b
gabriella <gabriella.mosca@gmail.com>
2020-01-13 20:06:15 +0100

    Addes plugin for MorphoMechanX to computer semi automatically cell type and principal elongation axes

---
+++ MorphoDynamX.py
@@ -0,0 +1,16 @@
+######## New MorphoDynamX session v2.0 r2-672-gb342f639: 2020-01-13 19:46:39
+
+Process.Mesh__System__Load('/home/gabriella/IMAGO_project/realData_shapeAnalysis/wt/st1-I/exported_EM_C_200/EM_C_200_3DCellTissue.mdxm', 'no', 'no', '0')
+Process.Tools__System__Save_View('/tmp/VLy3VLOx/FemMembranes.mdxv')
+Process.Mesh__System__Save('/home/gabriella/IMAGO_project/realData_shapeAnalysis/wt/st1-I/exported_EM_C_200/EM_C_200_3DCellTissue.mdxm', 'no', '0')
+Process.Tools__System__Save_View('/tmp/VLy3VLOx/FemMembranes.mdxv')
+Process.Model__CCF__60_Shape_Quantifier__04_Visualize_Shape_Field('CellShapeData', 'Draw cell anisotropy', '4')
+Process.Tools__System__Save_View('/tmp/VLy3VLOx/FemMembranes.mdxv')
+Process.Tools__System__Load_View('')
+Process.Mesh__System__Load('/home/gabriella/IMAGO_project/git_repo3DsemiAutolabelingAndAnisotropy/3DsemiAutoLabelingOvule/EM_C_188_cell220merged_3DtissueMorphogens.mdxm', 'no', 'no', '0')
+Process.Tools__System__Load_View('FemMembranes.mdxv')
+######## New MorphoDynamX session v2.0 r2-672-gb342f639: 2020-01-13 20:01:40
+Process.Tools__System__Load_View('FemMembranes.mdxv')
+Process.Model__CCF__60_Shape_Quantifier__04_Visualize_Shape_Field('CellShapeData', 'Draw cell anisotropy', '4')
+Process.Mesh__System__Save('/home/gabriella/IMAGO_project/git_repo3DsemiAutolabelingAndAnisotropy/3DsemiAutoLabelingOvule/EM_C_200_3DCellTissue.mdxm', 'no', '0')
+Process.Tools__System__Save_View('/home/gabriella/IMAGO_project/git_repo3DsemiAutolabelingAndAnisotropy/3DsemiAutoLabelingOvule/FemMembranes.mdxv')
